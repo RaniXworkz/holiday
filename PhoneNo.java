64f12fb525566cbd268889d3fe02f38337cba884
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-28 23:23:28 +0530

    PhoneNo

---
+++ PhoneNo.java
@@ -0,0 +1,9 @@
+class PhoneNo
+{
+public static void main(String[] args)
+{
+String []MobileNo={"8431935331","7338144269","8762902478","9591931134","7892564006","9902436835"};
+System.out.println(MobileNo[0]+","+MobileNo[1]+","+MobileNo[2]+","+MobileNo[3]+","+MobileNo[4]+","+MobileNo[5]);
+}
+}
+
