53c9611f1c2b03cbc4a45eaab87a45da27d74a1e
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-28 23:15:16 +0530

    Weekdays

---
+++ Weekdays.java
@@ -0,0 +1,11 @@
+class Weekdays
+{
+public static void main(String []args)
+{
+String []days={"Sun","Mon","Tue","wed","Thu","Fri","Sat"};
+for(int index=0; index<days.length; index=index+1)
+{
+System.out.println(days[index]);
+}
+}
+}
