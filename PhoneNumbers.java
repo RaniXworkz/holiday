e9fbddb40c0bfea567f019198600f1b82875ea07
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-28 23:19:36 +0530

    PhoneNumbers

---
+++ PhoneNumbers.java
@@ -0,0 +1,11 @@
+class phonenumbers
+{
+public static void main(String[] args)
+{
+String []PhoneNo={"8431935331","7338144269","8762902478","9591931134","7892564006","9902436835"};
+for(int index=0;index<PhoneNo.length;index=index+1)
+{
+System.out.println(PhoneNo[index]);
+}
+}
+}
