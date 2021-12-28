622b2973a9697b27f3815f1917510b60e11734ef
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-28 23:29:13 +0530

    Months

---
+++ Months.java
@@ -0,0 +1,11 @@
+class Months
+{
+public static void main(String []args)
+{
+String []Months={"January","February","March","April","May","June","July","August","September","October","November","December"};
+for(int index=0; index<Months.length; index=index+1)
+{
+System.out.println(Months[index]);
+}
+}
+}
