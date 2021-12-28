167c0cf7f92598a44aabbce3da4f55f8cba3ce06
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-28 21:14:55 +0530

    Demo

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo{
+public static void main(String []args){
+int id=50;
+char a='t';
+byte var=45;
+short age=450;
+long num=234567899;
+float b=2345;
+double z=6789;
+boolean y=true;
+System.out.println(id +  var + age + num + b + z);
+}
+}
