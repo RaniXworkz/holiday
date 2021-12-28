fcb02dd202d3999ed624eb8a8bc3c726110f6dce
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-28 21:16:56 +0530

    Lion

---
+++ Lion.java
@@ -0,0 +1,12 @@
+class Lion{
+public static void main(String []demo){
+int input = 7;
+int sum =0;
+for(int i=1;i<=input;i++){
+sum=sum+i;
+System.out.println("Sum after adding" +i+ "is:" +sum);
+}
+System.out.println();
+System.out.println("Sum of numbers till" +input+ "is" +sum);
+}
+}
