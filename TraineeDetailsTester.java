0f3111f3e8f9d51000e7961a1266b31461da3b17
RaniXworkz <96232620+RaniXworkz@users.noreply.github.com>
2022-01-04 10:43:51 +0530

    Add files via upload

---
+++ TraineeDetailsTester.java
@@ -0,0 +1,14 @@
+class TraineeDetailsTester
+{
+ public static void main(String []ar){
+ TraineeDetails.printAll();
+ TraineeDetails.updateName("Rani","Raji");
+ System.out.println("====================");
+ TraineeeDetails.printAll();
+ }
+ }
+ 
+  printALl[1]= rani
+  printALl[2]= rani
+  printALl[3]= deepa
+  
